0906bce31ccb706ecb4933d248f2fe17485ffe3d
slamdank22 <artem.call2@mail.ru>
2022-12-21 18:45:52 +0300

    JustMusic

---
+++ Assets/PlayAudioOnTiggerEnter.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayAudioOnTiggerEnter : MonoBehaviour
+{
+    public AudioClip clip;
+    private AudioSource source;
+    public string targetTag;
+
+    void Start()
+    {
+        source = GetComponent<AudioSource>();
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag(targetTag))
+        {
+            source.PlayOneShot(clip);
+        }
+    }
+}
